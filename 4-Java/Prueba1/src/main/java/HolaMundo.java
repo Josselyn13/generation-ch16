10b0c9e27b03429010887f421bf6ea3196d89743
Josselyn <jossm@ciencias.unam.mx>
2022-08-16 16:21:11 -0500

    Java

---
+++ 4-Java/Prueba1/src/main/java/HolaMundo.java
@@ -0,0 +1,8 @@
+public class HolaMundo {
+    public static void main(String[] args) {
+        //String miVariable = "Hola mundo Java";
+        String nombre = "Josselyn";
+        System.out.println("Saludos desde Java a: " + nombre);
+
+    }
+}
---
+++ 4-Java/ejemploJdk/Hola.java
@@ -0,0 +1,13 @@
+public class Hola{
+//esto es una clase del JDK de Java, public es un modificador
+
+
+    public static void main(String[] args) {
+        String miVariable = "Hola mundo java saludo a: ";
+        String miNombre = "Josselyn Ileana";
+        //es la declaración del método main
+        System.out.println(miVariable + miNombre);
+    }
+
+
+}
